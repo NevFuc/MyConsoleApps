83f009b0d98d1cea3305a083fdf3c288569c34be
NevFuc <nevio.fuchs@edu.gbsssg.ch>
2025-08-13 11:41:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
